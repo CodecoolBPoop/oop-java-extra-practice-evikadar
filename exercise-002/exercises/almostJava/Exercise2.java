ed22d33a18c333a8f075f686968d407d92988a9c
Evi Kadar <evi.kadar@gmail.com>
2018-11-27 12:22:22 +0100

    Exercise2 done

---
+++ exercise-002/exercises/almostJava/Exercise2.java
@@ -0,0 +1,12 @@
+package almostJava;
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        int num1 = 44;
+        int num2 = 9;
+        int result = num1 + num2;
+        System.out.printf("Result is %s.%n", result);
+
+    }
+}
